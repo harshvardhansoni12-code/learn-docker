9abac4f2ffc1b6f01e63c363d5477945a6d04544
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-07-13 10:42:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello harsh");
+});
+
+app.listen(3000, () => {
+  console.log("this is listening on port 3000");
+});
